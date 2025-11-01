96594d726309751cc9f7a153eeb8631c93a5cbf5
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-01 11:51:00 +0500

    logo updated

--- corebizlabs/src/assets/Image.js
+++
@@ -1,4 +0,0 @@
-const Images={
-    logo:new URL("corebizlabs.png",import.meta.url).href,
-}
-export default Images;
---
+++ corebizlabs/src/assets/Images/Image.js
@@ -0,0 +1,4 @@
+const Images={
+    logo:new URL("corebizlogo.png",import.meta.url).href,
+}
+export default Images;
