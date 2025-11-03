7ff5a3360ce593ca3e94f174c777d24cb12ee325
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-03 11:18:30 +0500

    updated-code

--- corebizlabs/src/App.jsx
+++ corebizlabs/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
