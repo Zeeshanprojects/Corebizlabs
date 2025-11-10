be9a71a847a44614e306292a9c2de88af269f0e7
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-10 17:13:40 +0500

    updated

--- corebizlabs/src/App.jsx
+++ corebizlabs/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About/>}/>
-            <Route path="/portfilio" element={<Portfolio/>}/>
+            <Route path="/portfolio" element={<Portfolio/>}/>
             <Route path="/contact" element={<Contact/>}/>
           </Routes>
           <Footer />
