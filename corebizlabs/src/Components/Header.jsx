2d245cdcf865736184a72a210af66eda57269409
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-10 16:23:04 +0500

    code-updated

--- corebizlabs/src/Components/Header.jsx
+++ corebizlabs/src/Components/Header.jsx
@@ -5,54 +5,59 @@ import Images from "../assets/Images/Image";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg main-navbar ">
-      <div className="container-fluid align-items-center">
+    <nav className="navbar navbar-expand-lg main-navbar">
+      <div className="container-fluid d-flex align-items-center justify-content-between">
+        
         {/* Logo */}
         <a className="navbar-brand d-flex align-items-center" href="#">
           <img className="navbar-logo" src={Images.logo} alt="logo" />
         </a>
 
-        {/* Toggle for mobile */}
+        {/* Mobile Toggle Button */}
         <button
-          className="navbar-toggler"
+          className="navbar-toggler border-0 p-2"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Center Menu */}
-        <div
-          className="collapse navbar-collapse justify-content-center"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav glass-menu py-2 px-3">
-            <li className="nav-item ">
-              <Link className="nav-link active highlight" to="/">
-                HOME 
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link " to="/about">
-                ABOUT US
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/portfilio">
-                PORTFOLIO
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                CONTACT
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {/* Collapsible Menu + CTA */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="d-flex flex-column flex-lg-row align-items-center w-100 justify-content-between mt-3 mt-lg-0">
+            
+            {/* Center Menu */}
+            <ul className="navbar-nav glass-menu mb-3 mb-lg-0 mx-auto">
+              <li className="nav-item">
+                <Link className="nav-link  highlight" to="/" onClick={() => window.scrollTo(0, 0)}>
+                  HOME
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about" onClick={() => window.scrollTo(0, 0)}>
+                  ABOUT US
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/portfolio" onClick={() => window.scrollTo(0, 0)}>
+                  PORTFOLIO
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact" onClick={() => window.scrollTo(0, 0)}>
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
 
-        {/* CTA Button */}
-        <button className="quote-btn">Get a Quote ↗</button>
+            {/* CTA Button */}
+            <button className="quote-btn">Get a Quote ↗</button>
+          </div>
+        </div>
       </div>
     </nav>
   );
