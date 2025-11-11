5f6c0dc9c091542b961bf0eadaa98c4a9b2c3ce8
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-11-11 11:19:08 +0500

    updated

--- corebizlabs/src/Components/Footer.jsx
+++ corebizlabs/src/Components/Footer.jsx
@@ -1,156 +1,139 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.css";
 
 export default function Footer() {
   return (
     <>
-      <hr className="underline"/>
-        <div className="container-fluid p-0">
-          {/* Footer */}
-          <footer
-            className="text-center text-lg-start text-white"
-            style={{ backgroundColor: "#0E0F11" }}
-          >
-            {/* Section: Social media */}
-            <section className="d-flex justify-content-between p-2"></section>
+      <hr className="underline" />
+      <div className="container p-0">
+        {/* Footer */}
+        <footer
+          className="text-center text-lg-start text-white"
+          style={{ backgroundColor: "#0E0F11" }}
+        >
+          {/* Section: Social media */}
+          <section className="d-flex justify-content-between p-2"></section>
 
-            <section className="">
-              <div className="container-fluid text-center text-md-start mt-5">
-                {/* Grid row */}
-                <div className="row mt-3">
-                  {/* Grid column */}
-                  <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    {/* Content */}
-                    <h6 className="text-uppercase fw-bold">CorebizLabs</h6>
-                    <hr
-                      className="mb-4 mt-0 d-inline-block mx-auto"
-                      style={{
-                        width: 60,
-                        backgroundColor: "#7c4dff",
-                        height: 2,
-                      }}
-                    />
-                    <p>
-                      Here you can use rows and columns to organize your footer
-                      content. Lorem ipsum dolor sit amet, consectetur
-                      adipisicing elit.
-                    </p>
-                  </div>
-                  {/* Grid column */}
-                  {/* Grid column */}
-                  <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    {/* Links */}
-                    <h6 className="text-uppercase fw-bold">Products</h6>
-                    <hr
-                      className="mb-4 mt-0 d-inline-block mx-auto"
-                      style={{
-                        width: 60,
-                        backgroundColor: "#7c4dff",
-                        height: 2,
-                      }}
-                    />
-                    <p>
-                      <a href="#!" className="text-white">
-                        MDBootstrap
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">
-                        MDWordPress
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">
-                        BrandFlow
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">
-                        Bootstrap Angular
-                      </a>
-                    </p>
-                  </div>
-                  {/* Grid column */}
-                  {/* Grid column */}
-                  <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                    {/* Links */}
-                    <h6 className="text-uppercase fw-bold">Useful links</h6>
-                    <hr
-                      className="mb-4 mt-0 d-inline-block mx-auto"
-                      style={{
-                        width: 60,
-                        backgroundColor: "#7c4dff",
-                        height: 2,
-                      }}
-                    />
-                    <p>
-                      <a href="#!" className="text-white">
-                        Your Account
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">
-                        Become an Affiliate
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">
-                        Shipping Rates
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">
-                        Help
-                      </a>
-                    </p>
-                  </div>
-                  {/* Grid column */}
-                  {/* Grid column */}
-                  <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    {/* Links */}
-                    <h6 className="text-uppercase fw-bold">Contact</h6>
-                    <hr
-                      className="mb-4 mt-0 d-inline-block mx-auto"
-                      style={{
-                        width: 60,
-                        backgroundColor: "#7c4dff",
-                        height: 2,
-                      }}
-                    />
-                    <p>
-                      <i className="fas fa-home mr-3" /> New York, NY 10012, US
-                    </p>
-                    <p>
-                      <i className="fas fa-envelope mr-3" /> info@example.com
-                    </p>
-                    <p>
-                      <i className="fas fa-phone mr-3" /> + 01 234 567 88
-                    </p>
-                    <p>
-                      <i className="fas fa-print mr-3" /> + 01 234 567 89
-                    </p>
-                  </div>
-                  {/* Grid column */}
+          <section className="">
+            <div className="container-fluid text-center text-md-start mt-5">
+              {/* Grid row */}
+              <div className="row mt-3">
+                {/* Grid column */}
+                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 mx-auto mb-4">
+                  {/* Content */}
+                  <h6 className="text-uppercase fw-bold">CorebizLabs</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: 60,
+                      backgroundColor: "#7c4dff",
+                      height: 2,
+                    }}
+                  />
+                  <p>
+                    Connecting businesses with their audiences and individuals
+                    with their dreams. Our path forward is one of continuous
+                    growth their dreams.
+                  </p>
                 </div>
-                {/* Grid row */}
+                {/* Grid column */}
+                {/* Grid column */}
+                <div className="col-sm-12 col-md-12 col-lg-2 col-xl-2 mx-auto mb-4">
+                  {/* Links */}
+                  <h6 className="text-uppercase fw-bold">Quicks links</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: 60,
+                      backgroundColor: "#7c4dff",
+                      height: 2,
+                    }}
+                  />
+                  <p>
+                    <a href="#!" className="text-white">
+                      About Us
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Service
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Contact Us
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Blog Post
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Pricing
+                    </a>
+                  </p>
+                </div>
+                {/* Grid column */}
+                {/* Grid column */}
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  {/* Links */}
+                  <h6 className="text-uppercase fw-bold">Contact</h6>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: 60,
+                      backgroundColor: "#7c4dff",
+                      height: 2,
+                    }}
+                  />
+                  <p>
+                    <a href="#!" className="text-white">
+                      +1 (949) 283-9554 A
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      david@corebizlabs.com
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Laguna Beach, California,
+United States
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-white">
+                      Help
+                    </a>
+                  </p>
+                </div>
+                {/* Grid column */}
+                {/* Grid column */}
+
+                {/* Grid column */}
               </div>
-            </section>
-            {/* Section: Links  */}
-            {/* Copyright */}
-            <div
-              className="text-center p-3"
-              style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-            >
-              © 2020 Copyright:
-              <a className="text-white" href="https://mdbootstrap.com/">
-                MDBootstrap.com
-              </a>
+              {/* Grid row */}
             </div>
-            {/* Copyright */}
-          </footer>
-          {/* Footer */}
-        </div>
-        {/* End of .container */}
-      </>
+          </section>
+          {/* Section: Links  */}
+          {/* Copyright */}
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2020 Copyright:
+            <a className="text-white" href="https://mdbootstrap.com/">
+              MDBootstrap.com
+            </a>
+          </div>
+          {/* Copyright */}
+        </footer>
+        {/* Footer */}
+      </div>
+      {/* End of .container */}
+    </>
   );
 }
