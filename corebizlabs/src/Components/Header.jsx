2b177bfe30c462c61914ca18bd7e4e371d3273ae
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2026-02-23 11:41:41 +0500

    updated-code

--- corebizlabs/src/Components/Header.jsx
+++ corebizlabs/src/Components/Header.jsx
@@ -7,7 +7,6 @@ export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg main-navbar">
       <div className="container-fluid d-flex align-items-center justify-content-between">
-        
         {/* Logo */}
         <a className="navbar-brand d-flex align-items-center" href="#">
           <img className="navbar-logo" src={Images.logo} alt="logo" />
@@ -29,26 +28,74 @@ export default function Header() {
         {/* Collapsible Menu + CTA */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="d-flex flex-column flex-lg-row align-items-center w-100 justify-content-between mt-3 mt-lg-0">
-            
             {/* Center Menu */}
             <ul className="navbar-nav glass-menu mb-3 mb-lg-0 mx-auto">
               <li className="nav-item">
-                <Link className="nav-link active highlight" to="/" onClick={() => window.scrollTo(0, 0)}>
+                <Link
+                  className="nav-link active highlight"
+                  to="/"
+                  onClick={() => window.scrollTo(0, 0)}
+                >
                   HOME
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about" onClick={() => window.scrollTo(0, 0)}>
+                <Link
+                  className="nav-link"
+                  to="/about"
+                  onClick={() => window.scrollTo(0, 0)}
+                >
                   ABOUT US
                 </Link>
               </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  SERVICES
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
               <li className="nav-item">
-                <Link className="nav-link" to="/portfolio" onClick={() => window.scrollTo(0, 0)}>
+                <Link
+                  className="nav-link"
+                  to="/portfolio"
+                  onClick={() => window.scrollTo(0, 0)}
+                >
                   PORTFOLIO
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/contact" onClick={() => window.scrollTo(0, 0)}>
+                <Link
+                  className="nav-link"
+                  to="/contact"
+                  onClick={() => window.scrollTo(0, 0)}
+                >
                   CONTACT
                 </Link>
               </li>
--- corebizlabs/src/main.jsx
+++ corebizlabs/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import 'bootstrap/js/dist/dropdown';
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App.jsx'
 
