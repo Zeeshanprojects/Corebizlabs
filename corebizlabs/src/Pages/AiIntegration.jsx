f78ea18e1873d833579d932bd862e5638acf31e0
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2026-02-23 10:40:55 +0500

    servies-updated

---
+++ corebizlabs/src/Pages/AiIntegration.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AiIntegration() {
+  return (
+    <div>AiIntegration</div>
+  )
+}
---
+++ corebizlabs/src/Pages/uiuxDesign.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function uiuxDesign() {
+  return (
+    <div>uiuxDesign</div>
+  )
+}
