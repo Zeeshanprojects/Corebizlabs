e294e770730479d394ffe4a392c3e3d466b741f7
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2026-02-24 16:21:21 +0500

    updated-code

--- corebizlabs/src/Components/Footer.jsx
+++ corebizlabs/src/Components/Footer.jsx
@@ -142,16 +142,16 @@ export default function Footer() {
             className="text-center p-3"
             style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
           >
-            © 2025 Copyright:
-            <a className="text-white" to="https://mdbootstrap.com/">
+            © 2025 Copyright{" "}
+            <a className="text-white" to="/">
               CorBizLabs.com
             </a>
           </div>
-          {/* Copyright */}
+     
         </footer>
-        {/* Footer */}
+    
       </div>
-      {/* End of .container */}
+
     </>
   );
 }
